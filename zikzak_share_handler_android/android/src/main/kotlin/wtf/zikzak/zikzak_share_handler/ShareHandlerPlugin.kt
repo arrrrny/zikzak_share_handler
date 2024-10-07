93898beadeb531f8f8b38b1cf9e0aa1c798c1b30
siddharthadevops <85803157+siddharthadevops@users.noreply.github.com>
2024-10-07 18:46:43 +0200

    Fix URL and PDF sharing issues in ShareHandlerPlugin
    This commit addresses two important issues in the ShareHandlerPlugin:
    
    1. URL and Document Sharing:
       - Fixed an issue where URLs were being discarded when shared along with documents.
       - Now properly handles both URLs and documents when shared simultaneously.
    
    2. WhatsApp PDF Sharing:
       - Resolved a bug where PDF files shared from WhatsApp were received without file extensions.
       - Implemented a fix to ensure PDF files retain their .pdf extension when shared.

--- zikzak_share_handler_android/android/src/main/kotlin/wtf/zikzak/zikzak_share_handler/ShareHandlerPlugin.kt
+++ zikzak_share_handler_android/android/src/main/kotlin/wtf/zikzak/zikzak_share_handler/ShareHandlerPlugin.kt
@@ -1,9 +1,13 @@
-package wtf.zikzak.zikzak_share_handler
+package com.shoutsocial.share_handler
 
+import android.content.ContentResolver
 import android.content.Context
 import android.content.Intent
 import android.graphics.BitmapFactory
 import android.net.Uri
+import android.provider.OpenableColumns
+import android.util.Log
+import android.webkit.MimeTypeMap
 
 import androidx.annotation.NonNull
 import androidx.core.app.Person
@@ -15,12 +19,18 @@ import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.embedding.engine.plugins.activity.ActivityAware
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.plugin.common.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
 import java.net.URLConnection
 
-private const val kEventsChannel = "wtf.zikzak.zikzak_share_handler/sharedMediaStream"
+private const val kEventsChannel = "com.shoutsocial.share_handler/sharedMediaStream"
 
 /** ShareHandlerPlugin */
-class ShareHandlerPlugin: FlutterPlugin, Messages.ShareHandlerApi, EventChannel.StreamHandler, ActivityAware, PluginRegistry.NewIntentListener {
+class ShareHandlerPlugin : FlutterPlugin, Messages.ShareHandlerApi, EventChannel.StreamHandler, ActivityAware,
+  PluginRegistry.NewIntentListener {
   private var initialMedia: Messages.SharedMedia? = null
   private var eventChannel: EventChannel? = null
   private var eventSink: EventChannel.EventSink? = null
@@ -42,40 +52,6 @@ class ShareHandlerPlugin: FlutterPlugin, Messages.ShareHandlerApi, EventChannel.
     Messages.ShareHandlerApi.setup(binding.binaryMessenger, null)
   }
 
-//  override fun getInitialSharedMedia(result: Result<SharedMedia>?) {
-//    result?.let { _result -> {
-//      initialMedia?.let { _media -> _result.success(_media) }
-//    } }
-//  }
-
-//  override fun recordSentMessage(media: SharedMedia) {
-//    val packageName = applicationContext.packageName
-//    val shortcutTarget = "$packageName.dynamic_share_target"
-//    val shortcutBuilder = ShortcutInfoCompat.Builder(applicationContext, media.conversationIdentifier ?: "").setShortLabel(media.speakableGroupName ?: "Unknown")
-//      .setIsConversation()
-//      .setCategories(setOf(shortcutTarget))
-//      .setIntent(Intent(Intent.ACTION_DEFAULT))
-//      .setLongLived(true)
-//
-//    val personBuilder = Person.Builder()
-//      .setKey(media.conversationIdentifier)
-//      .setName(media.speakableGroupName)
-//
-//    media.imageFilePath?.let {
-//      val bitmap = BitmapFactory.decodeFile(it)
-//      val icon = IconCompat.createWithAdaptiveBitmap(bitmap)
-//      shortcutBuilder.setIcon(icon)
-//      personBuilder.setIcon(icon)
-//    }
-//
-//    val person = personBuilder.build()
-//    shortcutBuilder.setPerson(person)
-//
-//    val shortcut = shortcutBuilder.build()
-//
-//    ShortcutManagerCompat.addDynamicShortcuts(applicationContext, listOf(shortcut))
-//  }
-
   override fun getInitialSharedMedia(result: Messages.Result<Messages.SharedMedia>?) {
     result?.success(initialMedia)
   }
@@ -90,7 +66,8 @@ class ShareHandlerPlugin: FlutterPlugin, Messages.ShareHandlerApi, EventChannel.
 //      putExtra("conversationIdentifier", media.conversationIdentifier)
 //    }
     val shortcutTarget = "$packageName.dynamic_share_target"
-    val shortcutBuilder = ShortcutInfoCompat.Builder(applicationContext, media.conversationIdentifier ?: "").setShortLabel(media.speakableGroupName ?: "Unknown")
+    val shortcutBuilder = ShortcutInfoCompat.Builder(applicationContext, media.conversationIdentifier ?: "")
+      .setShortLabel(media.speakableGroupName ?: "Unknown")
       .setIsConversation()
       .setCategories(setOf(shortcutTarget))
       .setIntent(intent)
@@ -152,41 +129,40 @@ class ShareHandlerPlugin: FlutterPlugin, Messages.ShareHandlerApi, EventChannel.
   }
 
   private fun handleIntent(intent: Intent, initial: Boolean) {
-    val attachments: List<Messages.SharedAttachment>?
-    val text: String?
-    when {
-      (intent.type?.startsWith("text") != true)
-              && (intent.action == Intent.ACTION_SEND
-              || intent.action == Intent.ACTION_SEND_MULTIPLE) -> { // Sharing images or videos
+    val attachments: List<Messages.SharedAttachment>? = try {
+      attachmentsFromIntent(intent)
+    } catch (e: Exception) {
+      Log.e("TAG", "Error parsing attachments", e)
+      null
+    }
 
-        attachments = attachmentsFromIntent(intent)
-        text = null
-      }
-      (intent.type == null || intent.type?.startsWith("text") == true)
-              && intent.action == Intent.ACTION_SEND -> { // Sharing text
-        text = intent.getStringExtra(Intent.EXTRA_TEXT)
-        attachments = if (text == null) {
-          attachmentsFromIntent(intent)
-        } else {
-          null
+    val text: String? = when (intent.action) {
+      Intent.ACTION_SEND, Intent.ACTION_SEND_MULTIPLE -> intent.getStringExtra(Intent.EXTRA_TEXT)
+      Intent.ACTION_VIEW -> intent.dataString
+      else -> null
+    }
+
+    val conversationIdentifier = intent.getStringExtra("android.intent.extra.shortcut.ID")
+      ?: intent.getStringExtra("conversationIdentifier")
+
+    if (attachments != null || text != null || conversationIdentifier != null) {
+      val mediaBuilder = Messages.SharedMedia.Builder()
+      attachments?.let { mediaBuilder.setAttachments(it) }
+      text?.let { mediaBuilder.setContent(it) }
+      conversationIdentifier?.let { mediaBuilder.setConversationIdentifier(it) }
+      val media = mediaBuilder.build()
+
+      if (initial) {
+        synchronized(this) {
+          initialMedia = media
         }
       }
-      intent.action == Intent.ACTION_VIEW -> { // Opening URL
-        attachments = null
-        text = intent.dataString
+
+      if (eventSink != null) {
+        eventSink?.success(media.toMap())
+      } else {
+        Log.w("TAG", "EventSink is not available")
       }
-      else -> {
-        attachments = null
-        text = null
-      }
-    }
-//    val conversationIdentifier = intent.getStringExtra(Intent.EXTRA_SHORTCUT_ID)
-    val conversationIdentifier = intent.getStringExtra("android.intent.extra.shortcut.ID") ?: intent.getStringExtra("conversationIdentifier")
-    if (attachments != null || text != null || conversationIdentifier != null) {
-//      val media = SharedMedia(attachments = attachments, content = text)
-      val media = Messages.SharedMedia.Builder().setAttachments(attachments).setContent(text).setConversationIdentifier(conversationIdentifier).build()
-      if (initial) initialMedia = media
-      eventSink?.success(media.toMap())
     }
   }
 
@@ -197,6 +173,7 @@ class ShareHandlerPlugin: FlutterPlugin, Messages.ShareHandlerApi, EventChannel.
         val uri = intent.getParcelableExtra<Uri>(Intent.EXTRA_STREAM) ?: return null
         return listOf(attachmentForUri(uri)).mapNotNull { it }
       }
+
       Intent.ACTION_SEND_MULTIPLE -> {
         val uris = intent.getParcelableArrayListExtra<Uri>(Intent.EXTRA_STREAM)
         val value = uris?.mapNotNull { uri ->
@@ -204,23 +181,114 @@ class ShareHandlerPlugin: FlutterPlugin, Messages.ShareHandlerApi, EventChannel.
         }?.toList()
         return value
       }
+
       else -> null
     }
   }
 
   private fun attachmentForUri(uri: Uri): Messages.SharedAttachment? {
-    val path = FileDirectory.getAbsolutePath(applicationContext, uri)
-    return if (path != null) {
-      val type = getAttachmentType(path)
-//      SharedAttachment(path = path, type = type)
-      Messages.SharedAttachment.Builder().setPath(path).setType(type).build()
+    val contentResolver = applicationContext.contentResolver
+
+    // Obtain the MIME type of the URI
+    val mimeType = contentResolver.getType(uri)
+
+    // Get the absolute path from the URI
+    val path = FileDirectory.getAbsolutePath(applicationContext, uri) ?: return null
+
+    val file = File(path)
+
+    // Check if the file name has an extension
+    if (file.extension.isNotEmpty()) {
+      // File has an extension; use it directly
+      val type = getAttachmentType(mimeType)
+      return Messages.SharedAttachment.Builder()
+        .setPath(file.absolutePath)
+        .setType(type)
+        .build()
     } else {
-      null
+      // File does not have an extension; copy it to cache with the correct extension
+
+      // Obtain the file name from the URI, including extension
+      val fileName = getFileNameFromUri(contentResolver, uri, mimeType) ?: return null
+
+      // Create a new file in the cache directory with the correct file name
+      val newFile = File(applicationContext.cacheDir, fileName)
+
+      // Copy the contents from the URI to the new file
+      val success = copyFile(contentResolver, uri, newFile)
+      if (!success) {
+        return null
+      }
+
+      // Determine the attachment type using the MIME type
+      val type = getAttachmentType(mimeType)
+
+      // Return the attachment with the path to the copied file
+      return Messages.SharedAttachment.Builder()
+        .setPath(newFile.absolutePath)
+        .setType(type)
+        .build()
     }
   }
 
-  private fun getAttachmentType(path: String?): Messages.SharedAttachmentType {
-    val mimeType = URLConnection.guessContentTypeFromName(path)
+  // Function to get the file name from the URI
+  private fun getFileNameFromUri(contentResolver: ContentResolver, uri: Uri, mimeType: String?): String? {
+    var fileName: String? = null
+    val cursor = contentResolver.query(uri, null, null, null, null)
+    cursor?.use { c ->
+      if (c.moveToFirst()) {
+        val nameIndex = c.getColumnIndex(OpenableColumns.DISPLAY_NAME)
+        if (nameIndex != -1) {
+          fileName = c.getString(nameIndex)
+        }
+      }
+    }
+
+    // Si no se pudo obtener el nombre del archivo, generar uno
+    if (fileName == null) {
+      fileName = "file_${System.currentTimeMillis()}"
+    }
+
+    // Si el nombre del archivo no tiene extensión, añadir una según el mimeType
+    val extension = fileName.substringAfterLast('.', "")
+    if (extension.isEmpty() || fileName.endsWith('.')) {
+      mimeType?.let {
+        val newExtension = MimeTypeMap.getSingleton().getExtensionFromMimeType(it)
+        if (!newExtension.isNullOrEmpty()) {
+          // Verificar si el nombre termina con un punto
+          fileName = if (fileName.endsWith('.')) {
+            fileName + newExtension
+          } else {
+            "$fileName.$newExtension"
+          }
+        }
+      }
+    }
+
+    return fileName
+  }
+
+  // Function to copy the file content from the URI to the destination file
+  private fun copyFile(contentResolver: ContentResolver, uri: Uri, destinationFile: File): Boolean {
+    return try {
+      contentResolver.openInputStream(uri)?.use { inputStream ->
+        FileOutputStream(destinationFile).use { outputStream ->
+          val buffer = ByteArray(8 * 1024) // 8KB buffer
+          var bytesRead: Int
+          while (inputStream.read(buffer).also { bytesRead = it } != -1) {
+            outputStream.write(buffer, 0, bytesRead)
+          }
+        }
+      }
+      true
+    } catch (e: Exception) {
+      e.printStackTrace()
+      false
+    }
+  }
+
+  // Function to determine the attachment type using the MIME type
+  private fun getAttachmentType(mimeType: String?): Messages.SharedAttachmentType {
     return when {
       mimeType?.startsWith("image") == true -> Messages.SharedAttachmentType.image
       mimeType?.startsWith("video") == true -> Messages.SharedAttachmentType.video
